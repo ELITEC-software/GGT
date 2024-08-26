eacc697303a5503d8ce56c8f08e98d84a5e567f4
JohnG <your.email@example.com>
2024-08-26 06:53:27 +0200

    scanner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import QRScanner from './QRScanner';
+import InstallPWA from './InstallPWA';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>QR Code Scanner PWA</h1>
+      <InstallPWA />
       <QRScanner />
     </div>
   );
---
+++ src/InstallPWA.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+
+function InstallPWA() {
+  const [supportsPWA, setSupportsPWA] = useState(false);
+  const [promptInstall, setPromptInstall] = useState(null);
+
+  useEffect(() => {
+    const handler = (e) => {
+      e.preventDefault();
+      setSupportsPWA(true);
+      setPromptInstall(e);
+    };
+    window.addEventListener('beforeinstallprompt', handler);
+
+    return () => window.removeEventListener('beforeinstallprompt', handler);
+  }, []);
+
+  const onClick = (evt) => {
+    evt.preventDefault();
+    if (!promptInstall) {
+      return;
+    }
+    promptInstall.prompt();
+  };
+
+  if (!supportsPWA) {
+    return null;
+  }
+
+  return (
+    <button
+      className="install-button"
+      onClick={onClick}
+    >
+      Install App
+    </button>
+  );
+}
+
+export default InstallPWA;
