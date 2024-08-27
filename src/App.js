8aa0c3b16cc696b371393121e596637b5b9b530f
JohnG <your.email@example.com>
2024-08-27 12:44:41 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -16,9 +16,12 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="absolute inset-0 bg-gradient-to-r from-[#8D1737] to-[#6D1229] shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
         <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-          <h1 className="text-2xl font-semibold mb-5">QR Code Scanner PWA</h1>
-          <InstallPWA />
+          <h1 className="text-center text-2xl font-semibold mb-5 text-[#8D1737]">QR Code Scanner PWA</h1>
+          <div className="flex justify-center">
+            <InstallPWA />
+          </div>
           <QRScanner />
         </div>
       </div>
--- src/InstallPWA.js
+++ src/InstallPWA.js
@@ -61,7 +61,7 @@ function InstallPWA() {
   if (installStatus === 'installable') {
     return (
       <button
-        className="mb-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className="mb-4 bg-[#8D1737] hover:bg-[#6D1229] text-white font-bold py-2 px-4 rounded shadow-md transition duration-300 ease-in-out transform hover:scale-105"
         onClick={() => {
           if (deferredPrompt) {
             deferredPrompt.prompt();
