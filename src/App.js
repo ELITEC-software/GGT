bd2ad9d07c74f5c50428e8f666b23a4a730df554
JohnG <your.email@example.com>
2024-08-29 15:18:31 +0200

    design úpravy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import QRScanner from './QRScanner';
 import Login from './Login';
@@ -8,8 +8,20 @@ import { IonApp } from '@ionic/react';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    // Check if the user is logged in when the component mounts
+    const loggedInStatus = localStorage.getItem('isLoggedIn');
+    setIsLoggedIn(loggedInStatus === 'true');
+  }, []);
+
   const handleLogin = () => {
     setIsLoggedIn(true);
+    localStorage.setItem('isLoggedIn', 'true');
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem('isLoggedIn');
   };
 
   return (
@@ -18,17 +30,16 @@ function App() {
         <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
           <Routes>
             <Route path="/" element={
-              isLoggedIn ? (
-                <Navigate to="/ggt-mobile" />
-              ) : (
-                <Login onLogin={handleLogin} />
-              )
+              isLoggedIn ? <Navigate to="/ggt-mobile" /> : <Login onLogin={handleLogin} />
             } />
             <Route path="/ggt-mobile" element={
               isLoggedIn ? (
                 <>
                   <QRScanner />
                   <InstallPWA />
+                  <button onClick={handleLogout} className="absolute top-4 right-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded shadow-md transition duration-300 ease-in-out transform hover:scale-105">
+                    Odhlásit se
+                  </button>
                 </>
               ) : (
                 <Navigate to="/" />
--- src/QRScanner.js
+++ src/QRScanner.js
@@ -32,7 +32,20 @@ function QRScanner() {
 
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-center text-2xl font-semibold mb-5 text-[#8D1737]">QR Kód Scaner</h1>
+      <div className="flex items-center justify-center mb-5">
+        <h1 className="text-center text-2xl font-semibold text-[#8D1737] mr-2">QR Kód Skener</h1>
+        <div className='pl-4'> 
+        <button
+          onClick={reloadCamera}
+          className="bg-[#8D1737] hover:bg-[#6D1229] text-white rounded-full p-2 transition duration-300 ease-in-out transform hover:scale-110"
+          aria-label="Obnovit kameru"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+          </svg>
+        </button>
+        </div>
+      </div>
       <div className="w-full max-w-md mb-4">
         {error ? (
           <div className="text-red-500 mb-2">{error}</div>
@@ -50,12 +63,6 @@ function QRScanner() {
         )}
       </div>
       <p className="text-lg font-medium mb-2">{result}</p>
-      <button
-        onClick={reloadCamera}
-        className="bg-[#8D1737] hover:bg-[#6D1229] text-white font-bold py-2 px-4 rounded"
-      >
-        Obnovit kameru
-      </button>
     </div>
   );
 }
